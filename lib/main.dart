0e623e78df29b032f80d764f822c30aeee715ef6
Carlos <cnoramb1@my.centennialcollege.ca>
2023-01-26 21:43:45 -0500

    Pay Calculator

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(title: const Text('Pay Calculator')),
+            body: Container(
+              child: Column(children: [
+                Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      size: 35.0,
+                    ),
+                  ],
+                ),
+              ]),
+            )));
+  }
+}
